29013e496e0ed17cd95426ab717d18daab4bfa0b
James Rowen <jamesrowen@gmail.com>
2012-09-30 04:49:32 -0500

    asdfasdf

--- js/dicom.js
+++ js/dicom.js
@@ -6,38 +6,18 @@ var markers = {};
 var users = [];
 var slices = [];
 var comments = [];
+var curSliceID = 0;
 
 $(loadData);
 $(loadThumbnails);
 
 function loadThumbnails(){
-	$.each(slices, function(i, s){
-		id = i + 1;
-
-		img = "<img src='" + s.fileName + "' style='width:80px; height:80px' />";
-		commentCount = "<span class='comment-count'>0</span>";
-		thumbReference = "<span class='thumb-reference'>" + id + "</span>";
-
-		thumbContent = img + commentCount + thumbReference;
-
-		thumbs.append("<li class='thumb' id='thumb-" + id + "'>" + thumbContent + "</li>");
-	});
-	
-	// add empty space
-	thumbs.append("<li style='height:600px'></li>");
-		
-	// callback when scrolling the thumbnails
-	$(".thumbnails").scroll(function()
-	{
-		var index = Math.floor($(this).scrollTop()/90);
-		
-		//$('#mainPic').attr('src', 'img/dicomi' + index + '.jpg');
-		sliceSelected(index);
-	});
 }
 
 function sliceSelected(id) {
+	curSliceID = id;
   markers = {};
+  $('#annotate span').remove();
   conversation.html("");
   loadSliceImage(id);
   loadComments(id);
@@ -51,7 +31,7 @@ function loadSliceImage(id) {
 function loadComments(id) {
   $.each(comments, function(i, c) {
     if (c.sliceId == id || c.inReplyTo == id) {
-      addComment(c);
+      showComment(c);
     }
   })
 }
@@ -70,8 +50,17 @@ function findComment(id) {
 }
 
 function addComment(comment) {
-  commentHtml = $("<li class='small-rounded-corners' id='comment-" + comment.id + "'>" + comment.text + "</li>");
+	comments.push(comment);
 
+  slices[comment.sliceId].commentCount += 1;
+  var thumb = $('#thumb-' + comment.sliceId);
+  var thumbCommentCount = thumb.find("span.comment-count");
+  thumbCommentCount.html(slices[comment.sliceId].commentCount);
+}
+
+function showComment(comment) {
+  commentHtml = $("<li class='small-rounded-corners' id='comment-" + comment.id + "'>" + comment.text + "</li>");
+  
   if (typeof(comment.inReplyTo) === "undefined") {
     conversation.append(commentHtml);
 
@@ -85,10 +74,6 @@ function addComment(comment) {
     inReplyTo.after(commentHtml);
     incrementMarkerCount(comment);
   }
-
-  var thumb = $('#thumb-' + comment.sliceId);
-  var thumbCommentCount = thumb.find("span.comment-count");
-  thumbCommentCount.html(parseInt(thumbCommentCount.html()) + 1);
 }
 
 function incrementMarkerCount(comment) {
@@ -111,8 +96,8 @@ function incrementMarkerCount(comment) {
 
 function addMarkers() {
   $.each(markers, function(commentId, markerAttributes) {
-    slice.addAnnotations(function(attributes) {
-      return newMarkerElement(commentId, attributes);
+    $('#annotate').addAnnotations(function(attributes) {
+		return $(document.createElement('span')).addClass('black circle note').html(markerAttributes.count);
     }, [markerAttributes]);
   })
 }
@@ -122,8 +107,31 @@ function newMarkerElement(commentId, markerAttributes) {
 }
 
 
+function blackNote() {
+	
+	var text = $(document.createElement('input')).attr('type', 'button').click(function() {addAnnotation()});
+	var span = $(document.createElement('span')).addClass('black circle note').append(text);
+	
+	return span;
+}
+
+function addAnnotation() {
+	var notes = $('#annotate span:last-child').seralizeAnnotations();
+	addComment({
+		id: comments.length + 1,
+		text: "Sed malesuada gravida nulla quis varius. Aliquam faucibus nulla nec ante rutrum dapibus. Phasellus porttitor gravida faucibus. Nulla imperdiet pellentesque.",
+		sliceId: curSliceID,
+		userId: 1,
+		x: notes[0].x,
+		y: notes[0].y
+	});
+	showComment(comments[comments.length - 1]);
+}
 
 function loadData(){
+
+	$('#annotate').annotatableImage(blackNote);
+		
   users.push({ name: "Spencer", id: 1 })
   users.push({ name: "James", id: 2 })
   users.push({ name: "Sri", id: 3 })
@@ -137,8 +145,31 @@ function loadData(){
       fileName: "img/dicomi" + i + ".jpg"
     });
   }
+  
+	$.each(slices, function(i, s){
 
-  comments.push({
+		img = "<img src='" + s.fileName + "' style='width:80px; height:80px' />";
+		commentCount = "<span class='comment-count'>0</span>";
+		thumbReference = "<span class='thumb-reference'>" + (i + 1) + "</span>";
+
+		thumbContent = img + commentCount + thumbReference;
+
+		thumbs.append("<li class='thumb' id='thumb-" + i + "'>" + thumbContent + "</li>");
+	});
+	
+	// add empty space
+	thumbs.append("<li style='height:600px'></li>");
+		
+	// callback when scrolling the thumbnails
+	$(".thumbnails").scroll(function()
+	{
+		var index = Math.floor($(this).scrollTop()/90);
+		
+		//$('#mainPic').attr('src', 'img/dicomi' + index + '.jpg');
+		sliceSelected(index);
+	});
+
+  addComment({
     id: 1,
     text: "Sed malesuada gravida nulla quis varius. Aliquam faucibus nulla nec ante rutrum dapibus. Phasellus porttitor gravida faucibus. Nulla imperdiet pellentesque.",
     sliceId: 1,
@@ -147,7 +178,7 @@ function loadData(){
     y: 0.71
   })
 
-  comments.push({
+  addComment({
     id: 2,
     text: "Quisque laoreet orci in purus vulputate aliquam. Fusce nulla nunc, faucibus sed lacinia sit amet, luctus in lacus. Maecenas ullamcorper.", 
     sliceId: 1,
@@ -155,7 +186,7 @@ function loadData(){
     inReplyTo: 1
   })
 
-  comments.push({
+  addComment({
     id: 3,
     text: "Aliquam erat volutpat. Ut ut sagittis mi. Mauris leo mauris, tempor vel tincidunt ut, auctor et felis. Aenean eu turpis.", 
     sliceId: 1,
@@ -163,7 +194,7 @@ function loadData(){
     inReplyTo: 1
   })
 
-  comments.push({
+  addComment({
     id: 4,
     text: "Phasellus nec orci non risus pulvinar euismod. Sed felis sem, mollis a porttitor nec, auctor sed libero. Nullam dignissim porta.",
     sliceId: 2,
@@ -172,7 +203,7 @@ function loadData(){
     y: 0.81
   })
 
-  comments.push({
+  addComment({
     id: 5,
     text: "Donec tristique aliquet massa, accumsan porttitor magna vehicula quis. Maecenas convallis tincidunt justo ut accumsan. Etiam et blandit lacus. Cras.",
     sliceId: 2,
@@ -180,7 +211,7 @@ function loadData(){
     inReplyTo: 4
   })
 
-  comments.push({
+  addComment({
     id: 6,
     text: "Aliquam dolor lacus, viverra ut lobortis quis, laoreet nec elit. Suspendisse quis tortor nisi, nec commodo est. Mauris et dolor.",
     sliceId: 2,
@@ -188,35 +219,35 @@ function loadData(){
     inReplyTo: 4
   })
 
-  comments.push({
+  addComment({
     id: 7,
     text: "Sed nibh risus, dignissim ac blandit eget, iaculis sed sapien. Etiam at lacus nulla, id pharetra dui. Sed vulputate bibendum.",
     sliceId: 3,
     userId: 5
   })
 
-  comments.push({
+  addComment({
     id: 8,
     text: "Etiam sollicitudin libero vel tellus condimentum hendrerit vulputate magna adipiscing. Cras tincidunt nulla a mi vulputate sit amet pellentesque lorem.",
     sliceId: 4,
     userId: 4
   })
 
-  comments.push({
+  addComment({
     id: 9,
     text: "Pellentesque at velit sit amet dolor lacinia pellentesque. Pellentesque neque urna, iaculis in viverra quis, consequat at mauris. Suspendisse ipsum.",
     sliceId: 4,
     userId: 2
   })
 
-  comments.push({
+  addComment({
     id: 10,
     text: "Ut vitae velit a neque faucibus consequat nec vel nisl. Vestibulum id mi sapien. Nulla nibh sem, iaculis ac dignissim.",
     sliceId: 5,
     userId: 1
   })
 
-  comments.push({
+  addComment({
     id: 11,
     text: "Fusce fermentum commodo ipsum, at vulputate massa rutrum non. Mauris nec massa tellus, ut congue justo. Vestibulum eget lorem non.",
     sliceId: 6,
@@ -225,7 +256,7 @@ function loadData(){
     y: 0.37
   })
 
-  comments.push({
+  addComment({
     id: 12,
     text: "Pellentesque porta ultricies dapibus. Duis porttitor porta lacus id cursus. Integer dignissim aliquam enim at sollicitudin. Integer vel neque dui.",
     sliceId: 6,
@@ -233,7 +264,7 @@ function loadData(){
     inReplyTo: 11
   })
 
-  comments.push({
+  addComment({
     id: 13,
     text: "Nullam vestibulum nisl eget odio feugiat ac posuere lacus posuere. Proin iaculis faucibus nisl. Etiam ultrices, risus a accumsan laoreet.",
     sliceId: 6,
@@ -241,7 +272,7 @@ function loadData(){
     inReplyTo: 11
   })
 
-  comments.push({
+  addComment({
     id: 14,
     text: "Aliquam convallis est vel risus vestibulum eget volutpat lectus facilisis. Morbi non sollicitudin urna. Curabitur faucibus, diam sit amet semper.",
     sliceId: 7,
@@ -250,7 +281,7 @@ function loadData(){
     y: 0.91
   })
 
-  comments.push({
+  addComment({
     id: 15,
     text: "Pellentesque nulla est, rhoncus vel vulputate sed, tempor nec urna. Morbi luctus tellus porta lacus ultricies bibendum in at dui.",
     sliceId: 7,
